5f4f16252428459a2757b6a0b00b74c57c309cc3
DaniliJog <Danili.jog@Gmail.com>
2018-06-01 20:55:29 +0200

    Help with all arguments

--- oshit/config.py
+++ oshit/config.py
@@ -6,4 +6,35 @@ import argparse
 class Config:
     def __init__(self, oSHIT):
         parser = argparse.ArgumentParser()
-        parser.parse_args()
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument("-s",
+                           "--send",
+                           action="store_true")
+        group.add_argument("-r",
+                           "--recv",
+                           action="store_true")
+        parser.add_argument("-c", "--crypto",
+                            type=str,
+                            help="crypto-type")
+        parser.add_argument("-f", "--file",
+                            type=str,
+                            help="/path/to/file")
+        parser.add_argument("-o", "--output",
+                            type=str,
+                            help="/path/to/outputfile")
+        parser.add_argument("-i",
+                            "--introducer",
+                            type=str,
+                            help="introducer server address")
+        parser.add_argument("-p",
+                            "--password",
+                            type=str,
+                            help="passphrase")
+        args = parser.parse_args()
+
+        if args.send:
+            print("send")
+        elif args.recv:
+            print("recv")
+        else:
+            print("You have to be either sender or reciver")
