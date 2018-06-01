9a0057ac8796303f380230cb3b7f795fba2e6ad5
AsbjornOlling <asbjornolling@gmail.com>
2018-06-01 21:56:54 +0200

    added logging utility

---
+++ oshit/logger.py
@@ -0,0 +1,31 @@
+
+## Logger
+# Simple logging utility
+class Logger():
+    def __init__(self, oSHIT, loglevel):
+        self.oSHIT = oSHIT
+        self.config = oSHIT.config
+
+        self.loglevel = loglevel
+
+        # TODO: add path to logfile to config object
+        # when config object is functional
+        logfilepath = "log"
+        self.logfile = open(logfilepath, 'a')
+
+
+    def log(self, loglevel, logstring):
+        """ Main logging function. called from everywhere """
+        # filter according to loglevel
+        if self.loglevel >= loglevel:
+            if loglevel == 0:
+                output = "[ERROR] " + logstring
+            elif loglevel == 1:
+                output = "[INFO] " + logstring
+            elif loglevel == 2:
+                output = "[DEBUG] " + logstring
+            elif loglevel == 3:
+                output = "[WTF] " + logstring
+
+            self.logfile.write(logstring + "\n")
+            print(output)
