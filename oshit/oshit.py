ee1c9da970fbdca962a701426966025f67e861f3
DaniliJog <Danili.jog@Gmail.com>
2018-06-01 18:14:54 +0200

    Some startup on this class

--- oshit/oshit.py
+++ oshit/oshit.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
 
+import transport
+import crypto
+import packet
+import filehandler
+
+
+class oSHIT:
+
+    def __init__(self):
+        self.transport = transport.Transport(self)
+
+        self.crypto = crypto.Crypto(self)
+
+        self.packet = packet.Packet(self)
+
+        self.filehandler = filehandler.Filehandler(self)
+
+
+
+
+
+
+
+
+
+
+
 if __name__ is '__main__':
     pass
