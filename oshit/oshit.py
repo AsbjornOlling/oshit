1ac2cc86cf5a95befa2ab56a13066b6764cec62a
AsbjornOlling <asbjornolling@gmail.com>
2018-06-01 18:18:10 +0200

    started transport. cleaned up linebreaks in oshit.py

--- oshit/oshit.py
+++ oshit/oshit.py
@@ -7,24 +7,11 @@ import filehandler
 
 
 class oSHIT:
-
     def __init__(self):
         self.transport = transport.Transport(self)
-
         self.crypto = crypto.Crypto(self)
-
         self.packet = packet.Packet(self)
-
         self.filehandler = filehandler.Filehandler(self)
-
-
-
-
-
-
-
-
-
 
 
 if __name__ is '__main__':
--- oshit/transport.py
+++ oshit/transport.py
@@ -1,15 +1,43 @@
 
+import socket
+
 # Transport
 # Implementation of the oSHIT protocol
 # for tcp-like flow control and error control 
 # over udp.
-class Transport:
-    def __init__(self, oSHIT):
-        # parent object
-        self.oSHIT = oSHIT
-        self.config = oSHIT.config
+class Transport():
+    TEST_IP = "localhost"
+    TEST_PORT = 4222
 
-        # init fields
+    def __init__(self):
+        # parent object
+        #self.oSHIT = oSHIT
+        #self.config = oSHIT.config
+
+        # queue of packets
         self.packets = []
 
-        
+        # create socket
+        self.sock = self.create_socket()
+
+        # test send
+        print("SENDING")
+        self.sock.sendto(bytes("HELLO WORD", "utf-8"), 
+                         (self.TEST_IP, self.TEST_PORT))
+
+
+    def create_socket(self):
+        """ Create a UDP socket to the TEST IP"""
+        return socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+    
+    def get_timeout(self):
+        """ TODO
+        Calculates the optimal timeout in milis,
+        based on a moving average of the round trip time so far.
+        """
+        return 1000
+
+
+# JUST DEBUGGAN
+t = Transport()
