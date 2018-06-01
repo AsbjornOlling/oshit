7eb9aac0de52459d3de39dbeb8de1fb6b45fb573
AsbjornOlling <asbjornolling@gmail.com>
2018-06-01 22:12:43 +0200

    idk

--- oshit/oshit.py
+++ oshit/oshit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logger
 import transport
 import crypto
 import packet
@@ -9,7 +10,11 @@ import config
 
 class oSHIT:
     def __init__(self):
+        # internal utilities
         self.config = config.Config(self)
+        loglevel = 2  # TODO: loglevel in config.py
+        self.logger = logger.Logger(self, loglevel)
+
         self.transport = transport.Transport(self)
         self.crypto = crypto.Crypto(self)
         self.packet = packet.Packet(self)
--- oshit/transport.py
+++ oshit/transport.py
@@ -9,20 +9,31 @@ import pdb
 # for tcp-like flow control and error control 
 # over udp.
 class Transport():
-    TEST_IP = "localhost"
-    TEST_PORT = 4222
+    LOCAL_IP = "0.0.0.0"
+    LOCAL_PORT = 4222
+    TEST_IP = "127.0.0.1"
+    TEST_PORT = 5555
 
     def __init__(self, oSHIT):
-        # parent and config
+        # general utility imports
         self.oSHIT = oSHIT
         self.config = oSHIT.config 
-
+        self.logger = oSHIT.logger
+        self.logger.log(2, "Initializing transport object.")
 
         # queue of packets
         self.packets = []
 
         # create socket
         self.sock = self.create_socket()
+
+        ##
+        ## DEBUGGING SHIT from here on
+        ## Beware - this is barely code.
+        ##
+        # TEST CONNECT 
+        self.logger.log(2, "Connecting to " + self.TEST_IP + ":" + str(self.TEST_PORT))
+        self.sock.connect((self.TEST_IP, self.TEST_PORT))
 
         # test send
         print("SENDING")
@@ -32,9 +43,10 @@ class Transport():
         # test receive
         print("RECEIVING")
         while True:
-            message = self.sock.recv(65535)  # max size of udp packet
+            message = self.sock.recv(5)  # max size of udp packet
             print("GITPACKET")
             print(message)
+
 
     def create_socket(self):
         """ Create a UDP socket to the TEST IP"""
@@ -47,10 +59,6 @@ class Transport():
         """
         return 1000
 
-
-class Incoming:
-    def __init__(self):
-        pass
 
 if __name__ == '__main__':
     # JUST DEBUGGAN
