175a6fdb533e4b40e64ab8787ee397cc74d8823a
AsbjornOlling <asbjornolling@gmail.com>
2018-06-04 11:55:57 +0200

    simple udp socket read and write works

--- oshit/transport.py
+++ oshit/transport.py
@@ -1,23 +1,24 @@
 # utility imports
 import socket
 
-# python debugger
-import pdb
 
-# Transport
-# Implementation of the oSHIT protocol
-# for tcp-like flow control and error control 
-# over udp.
 class Transport():
-    LOCAL_IP = "0.0.0.0"
+    """ Implementation of the oSHIT protocol
+    for tcp-like flow control and error control
+    over udp.
+    """
+    # info for local socket
+    LOCAL_IP = ""
     LOCAL_PORT = 4222
+
+    # connect to this one
     TEST_IP = "127.0.0.1"
     TEST_PORT = 5555
 
     def __init__(self, oSHIT):
         # general utility imports
         self.oSHIT = oSHIT
-        self.config = oSHIT.config 
+        self.config = oSHIT.config
         self.logger = oSHIT.logger
         self.logger.log(2, "Initializing transport object.")
 
@@ -27,37 +28,22 @@ class Transport():
         # create socket
         self.sock = self.create_socket()
 
-        ##
-        ## DEBUGGING SHIT from here on
-        ## Beware - this is barely code.
-        ##
+        # TEST READ
+        self.logger.log(2, "Starting test dataread")
+        msg, addr = self.sock.recvfrom(1024)
+        print(msg)
 
-        # TEST CONNECT 
-        self.logger.log(2, "Connecting to " + self.TEST_IP + ":" + str(self.TEST_PORT))
-        try:
-            self.sock.connect((self.TEST_IP, self.TEST_PORT))
-            self.logger.log(2, "Successfully connected.")
-        except:
-            self.logger.log(2, "Failed connect to" + self.TEST_IP + ":" + str(self.TEST_PORT))
-
-        # TEST SEND
-        message = "HELLO WORLD"
-        self.logger.log(2, "Test-sending " + message)
-        self.sock.sendto(bytes(message, "utf-8"),
-                         (self.TEST_IP, self.TEST_PORT))
-
-        # test receive
-        chunk = 5  # TODO set to max size of udp packet
-        self.logger.log(2, "Entering infinite " + str(chunk) + " byte print loop")
-        while True:
-            message = self.sock.recv(chunk)
-            print("GITPACKET")
-            print(message)
+        # TEST WRITE
+        self.logger.log(2, "Starting test datawrite")
+        self.sock.connect((self.TEST_IP, self.TEST_PORT))
+        self.sock.sendall(bytes("wow", "utf-8"))
 
     def create_socket(self):
         """ Create a UDP socket to the TEST IP"""
-        return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-    
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        sock.bind((self.LOCAL_IP, self.LOCAL_PORT))
+        return sock
+
     def get_timeout(self):
         """ TODO
         Calculates the optimal timeout in milis,
@@ -68,4 +54,5 @@ class Transport():
 
 if __name__ == '__main__':
     # JUST DEBUGGAN
-    t = Transport()
+    import oshit
+    t = Transport(oshit.oSHIT())
