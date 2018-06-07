7809953f6e3d18f25c6979ff625b850ccc1000c4
AsbjornOlling <asbjornolling@gmail.com>
2018-06-07 16:52:45 +0200

    started structure in transport / packet system

--- oshit/packet.py
+++ oshit/packet.py
@@ -1,1 +1,14 @@
 
+class Packet():
+    """ Object representing one packet.
+    Has fields to represent flags in the packet header,
+    method to encrypt / decrypt the packet.
+    """
+    def __init__(self):
+        pass
+
+    def get_bytes(self):
+        """ Get bytes of entire packet
+        Header, payload and all.
+        """
+        pass
