8856e0ede4a5133980c79c57ac72c8d303546a21
DaniliJog <Danili.jog@Gmail.com>
2018-06-08 16:29:58 +0200

    FileHandler now both read and write to files

--- oshit/filehandler.py
+++ oshit/filehandler.py
@@ -1,3 +1,6 @@
+import os
+
+
 class FileHandler():
     """ Class handling read and write to files.
     Uses default python open and read.
@@ -13,15 +16,27 @@ class FileHandler():
         self.logger.log(2, "Initializing filehandler object.")
 
         # Open the file
-        self._file = open(self.config["file"], 'rb')
+        self._readfile = open(self.config["file"], 'rb')
 
     def read_chunk(self):
         """ Use the path from config to read chunks of file.
         returns the read chunks as bytes, and EOF as a boolean.
         """
-        self.eof = False
-        chunk = self._file.read(self.PSIZE)
-        if not chunk:
-            self.eof = True
-            self._file.close()
-        return(chunk, self.eof)
+        try:
+            eof = False
+            readchunk = self._readfile.read(self.PSIZE)
+            if not readchunk:
+                eof = True
+                self._readfile.close()
+            return(readchunk, eof)
+        except:
+            print("Called read_chunk after EOF")
+
+    def write_chunk(self, writedata):
+        """ Takes a datachunk as argument and write it to a file.
+        It appends the data if the file already exits.
+        """
+        mode = 'ab' if os.path.exists(self.config['output']) else 'wb'
+        _writefile = open(self.config['output'], mode)
+        _writefile.write(writedata)
+        _writefile.close()
